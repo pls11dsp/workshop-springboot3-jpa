eb6f1d38ba7d58f2207be70251a917f59c86f30a
Pedro Luis Somera <pls11@dspcontroles.com.br>
2024-03-30 11:43:08 -0300

    User entity and resource

---
+++ src/main/java/com/dspcontroles/course/resourses/UserResourse.java
@@ -0,0 +1,21 @@
+package com.dspcontroles.course.resourses;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dspcontroles.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResourse {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+		
+	}
+
+}
