a0bcc6d0991406c90ad3afcb74259d2ae7d41820
Pedro Luis Somera <pls11@dspcontroles.com.br>
2024-04-06 10:47:47 -0300

    Service layer, component registration

--- src/main/java/com/dspcontroles/course/services/UserService.java
+++ src/main/java/com/dspcontroles/course/services/UserService.java
@@ -1,12 +1,26 @@
 package com.dspcontroles.course.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dspcontroles.course.entities.User;
 import com.dspcontroles.course.repositories.UserRepository;
 
+@Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
 
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
 }
