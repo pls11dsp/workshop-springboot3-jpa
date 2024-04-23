0738083deb40b1b20f83e475241c7eec9e15d8af
Pedro Luis Somera <pls11@dspcontroles.com.br>
2024-04-23 05:39:30 -0300

    OrderItem, many-to-many association with extra attributes Parte 2

--- src/main/java/com/dspcontroles/course/entities/Order.java
+++ src/main/java/com/dspcontroles/course/entities/Order.java
@@ -2,7 +2,11 @@ package com.dspcontroles.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Objects;
+import java.util.Set;
 
 import com.dspcontroles.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,6 +17,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -32,6 +37,9 @@ public class Order implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order() {
 	}
@@ -76,6 +84,10 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public Set<OrderItem> getItems(){
+		return items;
 	}
 
 	@Override
