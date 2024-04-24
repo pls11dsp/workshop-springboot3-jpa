678617b85e182ce8d538eb31253f25d591bc80e6
Pedro Luis Somera <pls11@dspcontroles.com.br>
2024-04-24 17:19:14 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/dspcontroles/course/entities/OrderItem.java
+++ src/main/java/com/dspcontroles/course/entities/OrderItem.java
@@ -42,7 +42,7 @@ public class OrderItem implements Serializable{
 	}
 	
 	public Product getProduct() {
-		return id.getProduct();
+	return id.getProduct();
 	}
 	
 	public void setProduct(Product product) {
